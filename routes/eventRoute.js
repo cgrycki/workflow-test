1ac2441dca007fa4bfcc2335b43a7ecfb80e3756
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-06-12 12:34:07 -0500

    Add routes.

---
+++ routes/eventRoute.js
@@ -0,0 +1,11 @@
+/**
+ * Events Router
+ */
+const express = require('express');
+var    router = express.Router();
+//const EventModel = require('../models/eventModel');
+
+/* Router Parameters + Validation -------------------------------------------*/
+
+
+module.exports = router;
