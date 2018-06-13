84c9c676c80bb2cd4675c42972e7ee4e08a842ed
Larson <rlrson@uiowa.edu>
2018-06-13 16:51:00 -0500

    Add functions and middleware to Database model.

--- models/index.js
+++ models/index.js
@@ -5,7 +5,6 @@
 var dynamo = require('dynamodb');
 var Joi    = require('Joi');
 
-
 /**
 * Events Model
 */
@@ -26,5 +25,81 @@ var EventModel = dynamo.define('Event', {
 
   tableName: process.env.DYNAMO_TABLE
 });
+
+
+/**
+ * Checks to see if a document with a given :Id exists in our DB
+ * @param {object} request Express HTTP request.
+ * @param {object} response Express HTTP response to act upon.
+ * @param {function} next Function after this middleware
+ */
+EventModel.checkEventMiddleware = function(request, response, next) {
+  // Previous middleware has confirmed an :Id param exists.
+  let id = request.params.id;
+
+  // Check existence
+  EventModel
+    .query(id)
+    .exec((error, data) => {
+      // Return errors
+      if (error) return response.status(400).json({ error: JSON.stringify(error) });
+      // Not found
+      else if (data.Items.length === 0) return response.status(204).json({ error: "Not found"});
+      // Exists, next!
+      else {
+        request.item = data.Items[0];
+        next();
+      };
+    });
+} 
+
+
+/**
+ * Router callback function for GET /events. Lists all events in database.
+ * @param {object} request Express HTTP request.
+ * @param {object} response Express HTTP response to act upon.
+ */
+EventModel.listEvents = function(request, response) {
+  return this
+    .scan()
+    .exec((error, data) => {
+      if (error) return response.status(400).json({ error: JSON.stringify(error) });
+      else return response.status(200).json(data.Items);
+    });
+};
+
+/**
+ * Wrapper function for POST /events.
+ * @param {object} request Express HTTP request.
+ * @param {object} response Express HTTP response to act upon.
+ */
+EventModel.saveEvent = function(request, response) {
+  // Gather params from request.
+  let params = request.body;
+
+  return this.create(params, (error, data) => {
+    if (error) return response.status(400).json({ error: JSON.stringify(error) });
+    else return response.status(201);
+  });
+};
+
+/**
+ * Middleware for POST /events. Uses next so we can stack more middleware after.
+ * @param {object} request Express HTTP request.
+ * @param {object} response Express HTTP response to act upon.
+ * @param {function} next Function after this middleware
+ */
+EventModel.saveEventMiddleware = function(request, response, next) {
+  // Gather params
+  let params = request.body;
+
+  // Create an event, returning an error if present otherwise going to the next().
+  EventModel.create(params, (error, data) => {
+    if (error) return response.status(400).json({ error: JSON.stringify(error) });
+    else next();
+  });
+};
+
+
 
 module.exports = EventModel;
