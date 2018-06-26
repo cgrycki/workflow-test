d4ae3effced6cfac15fca46b40ad66c7dadc0144
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-06-26 17:02:29 -0500

    And combine /auth route endpoints into one route/function.

--- auth/auth.routes.js
+++ auth/auth.routes.js
@@ -13,11 +13,11 @@ router.param('code', utils.validParamCode);
 
 /* RESTful endpoints -------*/
 // GET /auth -- Redirects to our login URL
-router.get('/', (req, resp) => resp.redirect(utils.getAuthURL()));
+//router.get('/', (req, resp) => resp.redirect(utils.getAuthURL()));
 
 // GET /auth/:code -- Authenticates code sent from Campus Login tools
-router.get('/:code', 
-  [ validateParams, utils.authenticateCode ],
+//   [ validateParams, utils.authenticateCode ],
+router.get('/', utils.authenticateCode, 
   (request, response) => response.status(200).redirect(process.env.FRONTEND_URI));
 
 // GET /auth/logout -- Ends a user's session and redirects them to the login URL.
