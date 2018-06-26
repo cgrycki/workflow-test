d1fe19e2d27c578c80db76dd25beb80186627d20
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-06-26 16:59:23 -0500

    Change param variable in auth.utils' authenticateCode

--- auth/auth.utils.js
+++ auth/auth.utils.js
@@ -145,7 +145,7 @@ function checkSession(request, response, next) {
 
 // Authentication handshake with the U. Iowa servers
 async function authenticateCode(request, response, next) {
-  const code = request.query.code;
+  const code = request.params.code;
   if (code) {
     // We 'know' that the request came from a whitelisted domain
     // So use the authentication code to obtain an OAuth2 token
