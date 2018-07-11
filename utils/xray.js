0fed10f59fa1eecc73b65a4c59427dcd25b56a56
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-07-11 13:24:05 -0500

    Update Xray to include request/response headers

--- utils/xray.js
+++ utils/xray.js
@@ -13,16 +13,24 @@ var app_name = 'workflow-test' || process.env.APP_NAME +'-'+ process.env.EENV;
 var startTrace = xrayAWS.express.openSegment(app_name);
 
 const requestTrace = (request, response, next) => {
-  console.log('Request trace: ', request);
-  console.log('Process env: ', process.env);
+  
   xrayAWS.captureAsyncFunc('send', function(subsegment) {
-      subsegment.addAnnotation("AWS_ACCESS_KEY_ID", `${process.env.AWS_ACCESS_KEY_ID}`);
-      subsegment.addAnnotation("AWS_SECRET_ACCESS_KEY", `${process.env.AWS_SECRET_ACCESS_KEY}`);
-      subsegment.addAnnotation("AWS_SESSION_TOKEN", `${process.env.AWS_SESSION_TOKEN}`);
-      subsegment.addAnnotation("AWS_REGION", `${process.env.AWS_REGION}`);
-      subsegment.addAnnotation("REQUEST_COOKIES", JSON.stringify(request.cookies));
-      subsegment.addAnnotation("REQUEST_SESSION", JSON.stringify(request.session));
-      subsegment.close();
+
+    // Capture AWS Credentials
+    subsegment.addAnnotation("AWS_ACCESS_KEY_ID", `${process.env.AWS_ACCESS_KEY_ID}`);
+    subsegment.addAnnotation("AWS_SECRET_ACCESS_KEY", `${process.env.AWS_SECRET_ACCESS_KEY}`);
+    subsegment.addAnnotation("AWS_SESSION_TOKEN", `${process.env.AWS_SESSION_TOKEN}`);
+    subsegment.addAnnotation("AWS_REGION", `${process.env.AWS_REGION}`);
+
+    // Capture cookies and session information
+    subsegment.addAnnotation("REQUEST_COOKIES", JSON.stringify(request.cookies));
+    subsegment.addAnnotation("REQUEST_SESSION", JSON.stringify(request.session));
+
+    // Capture request/response headers
+    subsegment.addAnnotation("REQUEST_HEADERS", JSON.stringify(request.headers));
+    subsegment.addAnnotation("RESPONSE_HEADERS", JSON.stringify(response.headers));
+
+    subsegment.close();
   });
   next();
 };
@@ -31,6 +39,6 @@ const requestTrace = (request, response, next) => {
 // app.use(xrayAWS.express.closeSegment());
 const endTrace = xrayAWS.express.closeSegment();
 
-exports.startTrace = startTrace;
+exports.startTrace   = startTrace;
 exports.requestTrace = requestTrace;
-exports.endTrace = endTrace;
+exports.endTrace     = endTrace;
